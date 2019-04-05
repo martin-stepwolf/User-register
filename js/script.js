b752addbace0fcf43c9e17f571ee71f3d3fc5ffb
IngCampos <15151130@mail.ita.mx>
2019-04-05 17:22:16 -0600

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,12 @@
+
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyCwfzmrTahppTl4EFwX1DywiqajR84P1JU",
+    authDomain: "martin-aa100.firebaseapp.com",
+    databaseURL: "https://martin-aa100.firebaseio.com",
+    projectId: "martin-aa100",
+    storageBucket: "martin-aa100.appspot.com",
+    messagingSenderId: "937744268341"
+  };
+  console.log(firebase.initializeApp(config));
+  console.log(firebase.app().name);
