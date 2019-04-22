572e225e3aa2e5082911ee4298669be5cbf06204
IngCampos <15151130@mail.ita.mx>
2019-04-22 14:54:04 -0500

    Escritura firebase y reacomodo

--- js/script.js
+++ js/script.js
@@ -1,11 +1,11 @@
-// Initialize Firebase
-var config = {
-  apiKey: "AIzaSyCwfzmrTahppTl4EFwX1DywiqajR84P1JU",
-  authDomain: "martin-aa100.firebaseapp.com",
-  databaseURL: "https://martin-aa100.firebaseio.com",
-  projectId: "martin-aa100",
-  storageBucket: "martin-aa100.appspot.com",
-  messagingSenderId: "937744268341"
-};
-console.log(firebase.initializeApp(config));
-console.log(firebase.app().name);
+var defaultApp = firebase.initializeApp(config);
+var defaultDatabase = defaultApp.database();
+function writeUserData(name) {
+  firebase
+    .database()
+    .ref("usuarios/")
+    .set({
+      username: name
+    });
+}
+
