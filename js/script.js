fabeeee2a286e64a9a7bf9114c6fda5c80046f5f
IngCampos <15151130@mail.ita.mx>
2019-04-22 16:53:22 -0500

    Lectura datos
    Se leen los datos de firebase, y se interpretan en el navegador

--- js/script.js
+++ js/script.js
@@ -1,11 +1,35 @@
 var defaultApp = firebase.initializeApp(config);
 var defaultDatabase = defaultApp.database();
+
 function writeUserData(name) {
+  id_next = firebase
+    .database()
+    .ref()
+    .child("users")
+    .push().key;
   firebase
     .database()
-    .ref("usuarios/")
+    .ref("users/" + name + "/") //agrega la ruta y una id unica
     .set({
+      id: id_next,
       username: name
     });
 }
+// writeUserData("raul");
 
+firebase
+  .database()
+  .ref("/users")
+  .once("value")
+  .then(function(data2) {
+    var data = Object.values(data2.val());
+    console.log(data);
+    for (a in data)
+      $(".row").append(
+        "<div class='alert alert-dark col-6'>Nombre: " +
+          data[a].username +
+          "<br>ID:" +
+          data[a].id +
+          " </div>"
+      );
+  }); //lectura de los datos
